1d0ce183935a246298c343f44b265a3d33787f16
tinhnh <46818248+tinhnh2@users.noreply.github.com>
2023-05-31 17:00:16 +0700

    Delete estimate_scripts directory

--- estimate_scripts/run_step3_4x.sh
+++
@@ -1,4 +0,0 @@
-for entry in `ls data`; do
-	iqtree-2.2.2.1 -seed 1 -T 32 -s data/$entry --prefix $entry -m "MIX{Q.step3.4x.1,Q.step3.4x.2,Q.step3.4x.3,Q.step3.4x.4}*R4" --no-seq-comp -wslmr
-done
-touch step3.iqtree
