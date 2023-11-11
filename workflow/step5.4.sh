9910f9c177f28b0932bc889357a5177789244333
tinhnh <46818248+tinhnh2@users.noreply.github.com>
2023-11-11 21:39:57 +0700

    Delete workflow directory

--- workflow/step5.4.sh
+++
@@ -1,2 +0,0 @@
-iqtree2 -seed 1 -st AA -T $1 -S out4  -te tree4.treefile --model-joint $2+FO --init-model Q.step3.4x.4  --prefix step5.4
-grep -A 22 "can be used as input for IQ-TREE" step5.4.iqtree | tail -n21 > Q.step5.4x.4
