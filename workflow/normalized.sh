ef1d6fcc63cf1a49106180f2ad9b65b84f829482
tinhnh <46818248+tinhnh2@users.noreply.github.com>
2023-11-11 10:31:14 +0700

    Delete workflow/normalized.sh

--- workflow/normalized.sh
+++
@@ -1,4 +0,0 @@
-python Normalize.py loop$1/step5/Q.step5.4x.1
-python Normalize.py loop$1/step5/Q.step5.4x.2
-python Normalize.py loop$1/step5/Q.step5.4x.3
-python Normalize.py loop$1/step5/Q.step5.4x.4
